f705270e415211ece704e4679509294224af5438
Arvin Fernandez <arvinf07@gmail.com>
2021-01-30 12:27:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+  const thanks = []
+  for(let i = 0; i < names.length; i++){
+    thanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return thanks
+}
+
+function countDown(num){
+  while(num >= 0){
+    console.log(num)
+    num--
+  }
+}
+
